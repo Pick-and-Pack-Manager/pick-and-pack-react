2968261d65691aaf08851a026d4b5026ea0a5f4a
vmmaturana <vmmaturana@gmail.com>
2022-10-03 17:11:39 +0700

    Log In HTML created

--- src/LogIn.js
+++ src/LogIn.js
@@ -5,24 +5,32 @@ class Login extends React.Component {
   state = {};
   render() {
     return (
-      <div className="card-body">
-        <form method="post" action="/auth/login">
-          <label className="form-label">Email</label>
-          <div className="input-group mb-3">
-            <input type="text" className="form-control" name="email" />
+      <div className="container">
+        {/*CARD (need to fix the size)*/}
+        <div className="card">
+          <div className="card-header">LogIn</div>
+          <div className="card-body">
+            {/*FORM*/}
+            <form method="post" action="/orders">
+              <label className="form-label">Email</label>
+              <div className="input-group mb-3">
+                <input type="text" className="form-control" name="email" />
+              </div>
+              <label className="form-label">Password</label>
+              <div className="input-group mb-3">
+                <input type="text" className="form-control" name="password" />
+              </div>
+              {/*BUTTON will take you to Orders if Authentification OK*/}
+              <button className="btn btn-success">Log In</button>
+            </form>
+            <p className="small inline py-2">
+              No access? Please click the link to let us know{" "}
+              <a className="card-link" href="/loginerror">
+                Click
+              </a>
+            </p>
           </div>
-          <label className="form-label">Password</label>
-          <div className="input-group mb-3">
-            <input type="text" className="form-control" name="password" />
-          </div>
-          <button className="btn btn-success">Log In</button>
-        </form>
-        <p className="small inline py-2">
-          New to Airbnb?{" "}
-          <a className="card-link" href="/auth/signup">
-            Signup
-          </a>
-        </p>
+        </div>
       </div>
     );
   }
