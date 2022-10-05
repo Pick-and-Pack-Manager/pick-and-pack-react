2f1c4bf9ba73e2f8b98a0a3957aaa64548c91d5f
kylejkent <kylejkent@gmail.com>
2022-10-05 15:44:04 +0700

    create login screen using react bootstrap

--- src/Profile.js
+++ src/Profile.js
@@ -2,6 +2,11 @@ import React from "react";
 import "./App.css";
 import Nav from "./NavComponent.js";
 
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+
 class Profile extends React.Component {
   state = {};
   render() {
