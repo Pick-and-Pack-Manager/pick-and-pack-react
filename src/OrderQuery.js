3c1f8db79acdfc86b10f2e3f4951fa4808f71bda
kylejkent <kylejkent@gmail.com>
2024-04-10 20:52:01 +0800

    add more details to order view and match with removed aliases

--- src/OrderQuery.js
+++ src/OrderQuery.js
@@ -44,21 +44,35 @@ class OrderQuery extends React.Component {
                 {this.state.orderData !== null && this.state.orderData.map(order => (
                     <div key={order.docEntry}>
                         <p style={{ margin: '2px' }}>
-                            <span>{order.orderNumber}</span>
-                            <span style={{ marginLeft: '10px' }}>{order.dealerName}</span>
+                            <span>{order.DocNum}</span>
+														<span style={{ marginLeft: '5px' }}>DocEntry: {order.DocEntry}</span>
+                            <span style={{ marginLeft: '10px' }}>{order.CardName}</span>
+														<span style={{ marginLeft: '10px' }}>Customer: {order.U_CustName}</span>
+														<span style={{ marginLeft: '10px' }}>Cust Address: {order.U_CustAdd}</span>
+														<span style={{ marginLeft: '10px' }}>Due Date: {order.DocDueDate}</span>
                         </p>
+												<p style={{ margin: '2px' }}>
+														<span style={{ marginLeft: '10px' }}>Released: {order.U_ASSMSTATUS}</span>
+														<span style={{ marginLeft: '10px' }}>Kitting Route:{order.U_KITROUTE}</span>
+														<span style={{ marginLeft: '10px' }}>Kitting Date: {order.U_Kitting}</span>
+														<span style={{ marginLeft: '10px' }}>Completing Route:{order.U_COMPROUTE}</span>
+														<span style={{ marginLeft: '10px' }}>Completing Date: {order.U_Completion}</span>
+												</p>
                         {/* Display orderLines for the current order */}
                         {order.orderLines && (
-                            <div>
-                                <p>Order Lines:</p>
-                                <ul>
+                            <div style={{ margin: '1px' }}>
+                                <p style={{ margin: '2px' }}>Order Lines:</p>
+                                <ol>
                                     {order.orderLines.map(line => (
-                                        <li key={line.lineDocEntry}>
-                                            <span>{line.itemCode}</span>
-                                            <span style={{ margin: '2px' }}>Description: {line.itemDescription}</span>
+                                        <li key={line.lineNum}>
+                                            <span>Line # {line.LineNum}</span>
+																						<span style={{ marginLeft: '2px' }}>VisOrder: {line.VisOrder}</span>
+																						<span style={{ marginLeft: '2px' }}>Item Code: {line.ItemCode}</span>
+                                            <span style={{ margin: '2px' }}>Description: {line.Dscription}</span>
+																						<span style={{ marginLeft: '2px' }}>Qty: {line.Quantity}</span>
                                         </li>
                                     ))}
-                                </ul>
+                                </ol>
                             </div>
                         )}
                         {/* Add additional properties rendering as needed */}
